d608ef49f0aa1173d211e26b672897b8d9d1a8a3
FaizStariwow <faizaverroes@gmail.com>
2024-11-04 20:40:18 +0700

    add role

---
+++ pages/murid/index.php
@@ -0,0 +1,76 @@
+<?php
+session_start();
+// include '../../connection/security.php'; 
+
+include '../../controller/admin/dashboard_controller.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <link rel="apple-touch-icon" sizes="76x76" href="../../assets/img/apple-icon.png">
+  <link rel="icon" type="image/png" href="../../assets/img/logoaverroes.png">
+  <title>
+    Siakad Averroes
+  </title>
+
+  <link href="https://fonts.googleapis.com/css?family=Inter:300,400,500,600,700,800" rel="stylesheet" />
+  <!-- Font Awesome Icons -->
+  <script src="https://kit.fontawesome.com/42d5adcbca.js" crossorigin="anonymous"></script>
+  <!-- CSS Files -->
+  <link id="pagestyle" href="../../assets/css/soft-ui-dashboard.css?v=1.1.0" rel="stylesheet" />
+  <link
+    rel="stylesheet"
+    href="https://site-assets.fontawesome.com/releases/v6.6.0/css/all.css">
+</head>
+
+<body class="g-sidenav-show  bg-gray-100">
+
+  <!-- Sidebar Content -->
+  <?php include '../layout/sidebar.php'; ?>
+  <!-- Sidebar End -->
+
+  <main class="main-content position-relative max-height-vh-100 h-100 border-radius-lg ">
+    <!-- Navbar -->
+    <?php include '../layout/header.php'; ?>
+    <!-- End Navbar -->
+
+    <div class="container-fluid py-4">
+
+      <!-- Content Start -->
+
+
+
+      <!-- Content End -->
+      <div class="container-fluid py-4">
+        
+        <!-- Footer Start -->
+        <?php include '../layout/footer.php'; ?>
+      </div>
+      <!-- Footer Start -->
+      <?php include '../layout/footer.php'; ?>
+    </div>
+    <!-- Footer End -->
+  </main>
+
+  <!-- Settings Start -->
+  <?php include '../layout/setting.php'; ?>
+  <!-- Settings End -->
+
+  <!--   Core JS Files   -->
+  <script src="./assets/js/core/popper.min.js"></script>
+  <script src="./assets/js/core/bootstrap.min.js"></script>
+  <script src="./assets/js/plugins/perfect-scrollbar.min.js"></script>
+  <script src="./assets/js/plugins/smooth-scrollbar.min.js"></script>
+  <script src="./assets/js/plugins/chartjs.min.js"></script>
+
+  <!-- Github buttons -->
+  <script async defer src="https://buttons.github.io/buttons.js"></script>
+  <!-- Control Center for Soft Dashboard: parallax effects, scripts for the example pages etc -->
+  <script src="./assets/js/soft-ui-dashboard.min.js?v=1.1.0"></script>
+</body>
+
+</html>
